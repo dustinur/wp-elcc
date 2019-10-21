66be073dad783edc6a89d80173c553fa32189b6d
DRobertson <dustinuriah@gmail.com>
2019-10-21 15:11:33 -0600

    Added footer

--- app/public/wp-content/themes/elcc/footer.php
+++ app/public/wp-content/themes/elcc/footer.php
@@ -1,6 +1,13 @@
-<footer>
-    <p>Test Footer</p>
+<!-- Footer -->
+<footer class="page-footer font-small">
+
+  <!-- Copyright -->
+  <div class="bold footer-copyright text-center py-3">©2019 Copyright: ELCC
+  </div>
+  <!-- Copyright -->
+
 </footer>
+<!-- Footer -->
 
 <?php wp_footer(); ?>
 
--- app/public/wp-content/themes/elcc/front-page.php
+++ app/public/wp-content/themes/elcc/front-page.php
@@ -13,24 +13,26 @@
 
         while ($blogposts->have_posts()) {
             $blogposts->the_post();
+
             ?>
 
             <div class="col-xs-12 col-md-6 col-lg-4">
                 <div class="card mt-4" style="width: 18rem;">
-                    <img src="images\bg-2.jpg" class="card-img-top" alt="..." />
+                    <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" class="card-img-top" />
                     <div class="card-body">
                         <h5 class="card-title"><?php the_title(); ?></h5>
                         <p class="card-text">
-                            <?php the_excerpt(); ?>
+                            <?php echo wp_trim_words(get_the_excerpt(), 30); ?>
                         </p>
                         <a href="<?php the_permalink(); ?>" class="btn btn-primary">View</a>
                     </div>
                 </div>
             </div>
 
-        <?php
-        }
+        <?php }
+            wp_reset_query();
         ?>
+
     </div>
 </div>
 
